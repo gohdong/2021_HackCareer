a1e71fc30a44c06cc04fe920959d0958ee0d26c3
gohdong <aldehf420@naver.com>
2021-11-18 22:28:25 +0900

    theme

---
+++ FrontEnd/lib/club_theme.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+
+class CluBColor {
+  static const Color mainColor = Color(0xff9596ff);
+  static const Color subRed = Color(0xffff7776);
+  static const Color subGreen = Color(0xff63cfbc);
+  static const Color darkGray = Color(0xff525252);
+  static const Color gray = Color(0xffa7a7a7);
+  static const Color lightGray = Color(0xffefefef);
+  static const Color ultraLightGray = Color(0xffefefef);
+}
+
+class CluBTextTheme {
+  static const TextStyle title = TextStyle();
+  static const TextStyle subTitle = TextStyle();
+  static const TextStyle body1 = TextStyle();
+  static const TextStyle body2 = TextStyle();
+  static const TextStyle button = TextStyle();
+  static const TextStyle headline = TextStyle();
+  static const TextStyle subHeadline = TextStyle();
+  static const TextStyle display1 = TextStyle();
+  static const TextStyle display2 = TextStyle();
+  static const TextStyle display3 = TextStyle();
+  static const TextStyle display4 = TextStyle();
+
+}
